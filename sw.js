2cea216f7cbd6c926d22649b4ec9cf78ec52d733
BRYAN0019 <2740218@yahoo.com.tw>
2026-05-27 08:49:05 +0800

    Initial commit

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'yiya-v1';
+const urlsToCache = [
+  '/',
+  '/app.html',
+  '/index.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache).catch(() => {
+          // 如果預快取失敗，繼續
+        });
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request)
+          .then(response => {
+            if (!response || response.status !== 200 || response.type === 'basic') {
+              return response;
+            }
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+            return response;
+          })
+          .catch(() => {
+            return caches.match('/index.html');
+          });
+      })
+  );
+});
